02983be128eaa104ef78e0ec055a65c9f5d775c6
Simon Dunkelman <sd9518@ic.ac.uk>
2022-03-22 17:28:55 +0000

    Made logo SVG, added privacy policy page

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,11 +1,19 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import "../styles/logo-filters.css";
+
+import fullLogo from "../images/logo-full-tomato.png";
+import logoSVG from "../images/full-logo-white.svg";
 
 const NavigationBar = () => (
   <header className="min-w-screen px-4">
     <nav className="py-4">
       <div className="flex justify-between items-center">
-        <h3 className="font-bold text-3xl hover:text-white"><Link to="/">Feeling Mutual</Link></h3>
+        {/*<h3 className="font-bold text-3xl hover:text-white"><Link to="/">Feeling Mutual</Link></h3>*/}
+        {/*<img className="w-32 h-auto" src={fullLogo} alt="Feeling Mutual logo" />*/}
+        <Link to="/">
+          <img className="h-auto w-40 tomato-logo white-hover" src={logoSVG} alt="Logo" />
+        </Link>
         <ul className="flex items-center w-auto space-x-12">
           <Link to="#what-we-do" className="no-underline font-light hover:text-white">
             <li>WHAT WE DO</li>
---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,170 @@
+import React from "react"
+import Layout from "../components/Layout"
+import NavigationBar from "../components/NavigationBar"
+import Section from "../components/Section"
+
+
+function PrivacyPolicy() {
+  return (
+    <Layout>
+      <Section cNames="bg-offwhite">
+        <NavigationBar />
+        <div className="space-y-4">
+          <h1 className="text-3xl text-center">Feeling Mutual Ltd.
+            Data Protection and Security Policy</h1>
+
+
+          <h2 className="text-2xl font-semibold">Privacy policy</h2>
+
+          <p>At Feeling Mutual we respect the privacy of visitors to our website www.feelingmutual.com. This policy is concerned with how we collect information, what we do with it and what controls you have over your personal information. </p>
+
+          <h4 className="text-xl font-semibold">Your Privacy</h4>
+          <p>Feeling Mutual is a strategic market research consultancy.  We engage with people to understand them better in order to help clients improve their products, services or marketing.</p>
+          We take our duty to process your personal information very seriously. This policy explains how we collect, manage, use and protect your personal information.
+          <br />
+          We may change this document from time to time to reflect the latest view of what we do with your information. Please check back frequently; you will be able to see if changes have been made by the date it was last updated.
+          <br />
+          Refer to the sections below for more details on how and why we use your personal information:
+          <ol>
+            <li>1. Who are we?</li>
+            <li>2. What personal information we collect and how we use it</li>
+            <li>3. Legitimate interests</li>
+
+            <li>4. Sharing your information</li>
+
+            <li>5. Retaining your information</li>
+
+            <li>6. Your details on the web</li>
+
+            <li>7. What are your rights?</li>
+
+            <li>8. How to contact us</li>
+          </ol>
+
+          <h4 className="text-xl font-semibold">1. Who are we?</h4>
+
+          In this policy references to the Feeling Mutual, or to ‘we’ or ‘us’ are to the Feeling Mutual which is a registered company in England and Wales.
+
+          <h4 className="text-xl font-semibold">2. What personal information we collect and how we use it</h4>
+
+          What we need
+
+          The company Feeling Mutual is what’s known as the ‘controller’ of the personal information you provide to us. We will usually collect basic personal information about you like your name, postal address, telephone number, email address and your payment details if you are purchasing from us.
+
+          Sometimes we will collect other information about you such as your date of birth, gender and other details. We will be very clear with you that we wish to collect such information, our reason for collecting such information, and we would only do so with your consent.
+
+          Why we need it
+
+          We collect your personal information in connection with specific activities, such as campaign updates, newsletter requests, registration or membership requests, product purchases, feedback, donations, competition entries, information you provide in public forums on our sites and applications and for the purpose of research that we might invite you to.
+
+          The information is either needed to fulfil your request or to enable us to provide you with a more personalised service. You don't have to disclose any of this information to browse our site. However, if you choose to withhold requested information, we may not be able to provide you with certain services.
+
+          Our marketing
+
+          Sometimes, with your consent, we will process your personal information to provide you with information about our work or our activities that you have requested or are expecting.
+
+          On other occasions, we may process personal information when we need to do this to fulfil a contract (for example, if you have purchased something from our web shop) or where we are required to do this by law or other regulations.
+
+          Feeling Mutual also processes your information when it is in our legitimate interests to do this and when these interests do not override your rights. Those legitimate interests include providing you with information on our products, campaigns, services, products, newsletter requests, feedback, competitions and other activities. Please see the section on 'Legitimate Interest' for more information.
+
+          How we obtain your details
+
+          We will also hold information about your details so that we can respect your preferences for being contacted by us.
+
+          We collect your personal information in a number of ways:
+      <ol>
+        <li>When you provide it to us directly.</li>
+
+        <li>When you provide permission to other organisations to share it with us (including Facebook or Twitter).</li>
+
+        <li>When we collect it as you use our websites or apps.</li>
+
+        <li>When you have given it to a third party and you have provided permission to pass your information on to us.</li>
+
+        <li>From publicly available sources (where possible) to keep your information up to date (e.g. the Post Office’s National Change of Address database).</li>
+      </ol>
+          We combine the information from these sources with the information you provide to us directly.
+
+          From time to time we may pay for the contact details of people who might be interested in hearing from us in future. Before we purchase contact information, we always check the wording used when your information was originally collected, to make sure that we only contact people who have actively expressed an interest in receiving information from third parties.
+
+            <h4 className="text-xl font-semibold">Children</h4>
+
+          If you are under 16 please ensure you obtain your parent/guardian's consent before sending any personal information to any website or Feeling Mutual. We do have activities for those under 18 so we may ask your age. Before taking part please ensure you speak to your parent or guardian.
+
+          Please note that we will not knowingly market to or accept orders for goods or services from persons aged under 18 years.
+
+          As a parent or guardian we encourage you to be aware of the activities in which your children are participating, both offline and online. If your children voluntarily disclose information, this may encourage unsolicited messages. We suggest that you discourage your child from providing any information without your consent.
+
+            <h4 className="text-xl font-semibold">Anything else?</h4>
+
+          All the personal information we process is processed within the EEA. However for the purposes of IT hosting and maintenance your information may be situated outside of the European Economic Area (EEA). This will be done in accordance with guidance issued by the Information Commissioner’s Office.
+
+          If you would like to change the way you hear from us or no longer wish to receive direct marketing communications from then use the contact form on our 'Contact Us' page.
+
+            <h4 className="text-xl font-semibold">3. Legitimate interests</h4>
+
+          We have a number of lawful reasons that mean we can use your personal information. One of these is something called 'legitimate interests'. Broadly speaking Legitimate Interests means we can process your personal information if:
+
+          We have a genuine and legitimate reason and we are not harming any of your rights and interests.
+
+          Please read the information we have provide in the 'Legitimate Interest' policy for more detailed information.
+
+            <h4 className="text-xl font-semibold">4. Sharing your information</h4>
+
+          We do not share your information with any other organisations or individuals unless we are when obliged to by law, for purposes of national security, taxation and criminal investigations and in the following instances:
+
+          If you have agreed that we may do so.
+          When we use other companies to provide services on our behalf, e.g. processing, mailing or delivering orders, answering customers’ questions about products or services, sending mail and emails, customer analysis, assessment, when using auditors/advisors or processing credit/debit card payments.
+          If we merge with another organisation to form a new entity, information may be transferred to the new entity.
+          We may disclose aggregate statistics about our site visitors, supporters, customers and sales to describe our services and operations to prospective partners, advertisers and other reputable third parties and for other lawful purposes, but these statistics won’t include any personally identifying information.
+          If we run an event in partnership with other named organisations your details may need to be shared. We will be very clear what will happen to your data when you register.
+          And, we will never sell or rent your personal information to other organisations.
+
+            <h4 className="text-xl font-semibold">5. Retaining your information</h4>
+
+          We hold your information only as long as necessary for each reason that we use it. We have provided some examples of the time we will keep your information in this paragraph but you can contact us for more information.
+
+          If you decide not to support Feeling Mutual any more or request that we have no further contact with you, we will keep some basic information in order to avoid sending you unwanted materials in the future and to ensure that we don’t accidentally duplicate information.
+
+          If you make a purchase from us, we will keep the purchase information for a period of seven years for accounting purposes.
+
+            <h4 className="text-xl font-semibold">6. What are your rights?</h4>
+
+          You have a number of rights about how the personal information you provide can be used. These are:
+
+          Transparency over how we use your personal information (right to be informed).
+          The ability to request a copy of the information we hold about you, which will be provided to you within one month (right of access).
+          Update or amend the information we hold about you if it is wrong (right of rectification).
+          Ask us to stop using your information (right to restrict processing).
+          Ask us to remove your personal information from our records (right to be 'forgotten').
+          Object to the processing of your information for marketing purposes (right to object).
+          Obtain and reuse your personal information for your own purposes (right to data portability).
+          Not be subject to a decision when it is based on automated processing (automated decision making and profiling).
+          If you would like to know more about your rights under the data protection law, you can find out more at the Information Commissioners Office website.
+
+          Remember, you can change the way you hear from us or withdraw your permission for us to processing your personal information at any time by using the form on our 'Contact Us' page.
+
+            <h4 className="text-xl font-semibold">7. How to contact us</h4>
+
+          <p>If you wish to talk through anything in our privacy policy, find out more about your rights or obtain a copy of the information we hold about you, please contact us (details at the bottom of this page), we will be happy to help.</p>
+
+          <p>If you wish to raise a complaint on how we have handled your personal information, you can contact our data protection officer Tom Woodnutt, who will investigate the matter. If you are not satisfied with our response or believe we are not processing your personal information in accordance with the law you can complain to the Fundraising Regulator or the Information Commissioner’s Office (ICO).</p>
+
+          <p>Our data protection officer can be contacted by writing to:</p>
+
+          <p>Data Protection Officer – Tom Woodnutt</p>
+
+          <p>Or by emailing: Tom@feelingmutual.com</p>
+
+          <p>If you wish to talk to us about anything in the policy or the information we hold about you please contact us:</p>
+
+          <p>Please note that calls to our supporter services team may be monitored or recorded.</p>
+
+          <p>This document was last updated: April 2018</p>
+        </div>
+      </Section>
+    </Layout>
+  )
+}
+
+export default PrivacyPolicy;
