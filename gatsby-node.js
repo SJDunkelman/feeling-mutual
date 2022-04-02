13b9aab2ef8b106e092119e344a55725252b1f5e
Simon Dunkelman <sd9518@ic.ac.uk>
2022-04-02 02:33:31 +0100

    Commented out bug in node. To be finished

--- gatsby-node.js
+++ gatsby-node.js
@@ -25,53 +25,53 @@ function dedupeTags(allMarkdownRemark) {
   return Array.from(uniqueTags)
 }
 
-exports.createPages = async ({ graphql, actions, reporter }) => {
-  // Query markdown files including data from frontmatter
-  const {
-    data: { allMarkdownRemark },
-  } = await graphql(`
-    query {
-      allMarkdownRemark {
-        edges {
-          node{
-          id
-          frontmatter {
-            title
-            description
-            date
-          }
-          fields {
-            slug
-          }
-        }
-      }
-    }
-  }
-  `)
-
-  const posts = allMarkdownRemark.edges;
-  const uniqueCategories = [...new Set(posts.map(function({ node }) {
-    return node.frontmatter.category;
-  }))];
-
-  // Create array of every category without duplicates
-  const uniqueTags = dedupeTags(allMarkdownRemark)
-  // Iterate over categories and create page for each
-  uniqueTags.forEach(tag => {
-    reporter.info(`Creating page: tag/${tag}`)
-    createPage({
-      path: `category/${tag}`,
-      component: require.resolve("./src/templates/CategoryList.js"),
-      // Create props for our CategoryList.js component
-      context: {
-        tag,
-        // Create an array of ids of articles in this category
-        ids: allMarkdownRemark.edges
-          .filter(({ node }) => {
-            return node.frontmatter.categories.includes(tag)
-          })
-          .map(({node}) => node.id),
-      },
-    })
-  })
-}
+// exports.createPages = async ({ graphql, actions, reporter }) => {
+//   // Query markdown files including data from frontmatter
+//   const {
+//     data: { allMarkdownRemark },
+//   } = await graphql(`
+//     query {
+//       allMarkdownRemark {
+//         edges {
+//           node{
+//           id
+//           frontmatter {
+//             title
+//             description
+//             date
+//           }
+//           fields {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   }
+//   `)
+//
+//   const posts = allMarkdownRemark.edges;
+//   const uniqueCategories = [...new Set(posts.map(function({ node }) {
+//     return node.frontmatter.category;
+//   }))];
+//
+//   // Create array of every category without duplicates
+//   const uniqueTags = dedupeTags(allMarkdownRemark)
+//   // Iterate over categories and create page for each
+//   uniqueTags.forEach(tag => {
+//     reporter.info(`Creating page: tag/${tag}`)
+//     createPage({
+//       path: `category/${tag}`,
+//       component: require.resolve("./src/templates/CategoryList.js"),
+//       // Create props for our CategoryList.js component
+//       context: {
+//         tag,
+//         // Create an array of ids of articles in this category
+//         ids: allMarkdownRemark.edges
+//           .filter(({ node }) => {
+//             return node.frontmatter.categories.includes(tag)
+//           })
+//           .map(({node}) => node.id),
+//       },
+//     })
+//   })
+// }
