41d44b43e6d1ca401f6d09c9c8cbbe2c9ae97c03
simondunkelman <simon@dunkelman.xyz>
2025-12-15 12:02:43 +0000

    removed all emailjs; updated to build to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,21 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   site: 'https://www.feelingmutual.com',
+
   integrations: [
     react(),
     tailwind(),
   ],
+
   vite: {
     ssr: {
       noExternal: ['react-fast-marquee'],
     },
   },
+
+  adapter: netlify(),
 });
